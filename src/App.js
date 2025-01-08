6f1897928fa911165ff7e43cb71cfdcfcc498578
Faith Osabutey <osabuteyfaith@gmail.com>
2025-01-08 23:18:06 +0000

    Changed the enter code to a weather app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <Weather />
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,3 +1,50 @@
+import "./App.css";
+
 export default function Weather() {
-  return "Hello from Weather and Fetty";
+  let currentCity = "Lagos";
+  let date = "Tuesday 10:00";
+  let description = "Moderate Rain";
+  let humidity = 87;
+  let wind = 7.2;
+  return (
+    <div className="Weather">
+      <header>
+        <form>
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            required
+            className="search-input"
+          />
+          <input type="submit" value="Search" className="search-button" />
+        </form>
+      </header>
+      <main>
+        <div className="current-weather">
+          <div>
+            <h1 className="current-city">{currentCity}</h1>
+            <p className="current-details">
+              <span>Last Updated:</span> {date} <br />
+              <span></span>
+              {description} <br />
+              Humidity: <strong>{humidity}%</strong>, Wind:{" "}
+              <strong>{wind}km/h</strong>
+            </p>
+          </div>
+          <div className="current-temperature">
+            <span className="current-temperature-icon">☀️</span>
+            <span
+              className="current-temperature-value"
+              id="current-temperature-value"
+            >
+              24
+            </span>
+            <span className="current-temperature-unit">
+              <a href="/">°C</a> | <a href="/">°F</a>
+            </span>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
